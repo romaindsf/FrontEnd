339f10476ffd829f8ecbe7fe2f3f83f0dde4b4f3
Romain Da Silva Faria <romaindsf@hotmail.fr>
2024-01-01 19:21:44 +0100

    POST request kinda works.

--- assets/JS/portfolio.js
+++ assets/JS/portfolio.js
@@ -41,21 +41,36 @@ if (logs != null) {
 };
 
 const addProjectForm = document.getElementById("add_project");
+const inputAddImage = document.querySelector("[type=file]");
+const DivPreviewImage = document.querySelector(".add_picture");
+inputAddImage.addEventListener("change", (event) => {
+    const image = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (event) => {
+        const divAddPictureContent = document.querySelectorAll(".add_picture *");
+        divAddPictureContent.forEach(element => {
+            element.style.display = "none";
+        });
+        const previewImage = document.createElement("img");
+        previewImage.src = event.target.result;
+        DivPreviewImage.appendChild(previewImage);
+        DivPreviewImage.style.padding = 0;
+    };
+    reader.readAsDataURL(image);
+});
+
 addProjectForm.addEventListener("submit", async function (event) {
     event.preventDefault();
-    const newProject = {
-        image: event.target.querySelector("[name=image]").value,
-        title: event.target.querySelector("[name=title]").value,
-        category: event.target.querySelector("[name=category]").value,
-    };
+    const newProject = new FormData();
+        newProject.append("image", event.target.querySelector("[name=image]").files[0]);
+        newProject.append("title", event.target.querySelector("[name=title]").value);
+        newProject.append("category", event.target.querySelector("[name=category]").value);
     const POSTrequest = await fetch("http://localhost:5678/api/works", {
         method: "POST",
-        body: JSON.stringify(newProject),
-        headers: {
-            "Authorization": `Bearer ${logs}`,
-            "Content-Type": "application/json"
-        },
+        body: newProject,
+        headers: {"Authorization": `Bearer ${logs}`},
     });
+    console.log(newProject);
     if (POSTrequest.status === 200) {console.log("Success!");}
     else {console.log("Erreur")};
 })
