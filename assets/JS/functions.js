b900d91671eda9989051732a25da993e8187c44b
Romain Da Silva Faria <romaindsf@hotmail.fr>
2024-01-04 18:58:37 +0100

    pretty much finished

--- assets/JS/functions.js
+++ assets/JS/functions.js
@@ -144,20 +144,50 @@ function displayAddProjectPopUp (popupBackground) {
     });
 }
 
-/*
-function addProject(event) {
-    const addProjectForm = document.getElementById("add_project");
-    const newProject = {
-        image: event.target.querySelector("[name=image]").value,
-        title: event.target.querySelector("[name=title]").value,
-        category: event.target.querySelector("[name=category]").value,
-    };
-    addProjectForm.addEventListener("submit", function (event) {
+function addImage (inputAddImage, DivPreviewImage) {
+    inputAddImage.addEventListener("change", (event) => {
+        const image = event.target.files[0];
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            const divAddPictureContent = document.querySelectorAll(".add_picture *");
+            divAddPictureContent.forEach(element => {
+                element.style.display = "none";
+            });
+            const previewImage = document.createElement("img");
+            previewImage.src = event.target.result;
+            DivPreviewImage.appendChild(previewImage);
+        };
+        reader.readAsDataURL(image);
+    });
+}
+
+async function addProject (addProjectForm, logs, responseWorks, projects) {
+    addProjectForm.addEventListener("submit", async function (event) {
         event.preventDefault();
-        console.log(newProject)
+        const newProject = new FormData();
+            newProject.append("image", event.target.querySelector("[name=image]").files[0]);
+            newProject.append("title", event.target.querySelector("[name=title]").value);
+            newProject.append("category", event.target.querySelector("[name=category]").value);
+        const POSTrequest = await fetch("http://localhost:5678/api/works", {
+            method: "POST",
+            body: newProject,
+            headers: {"Authorization": `Bearer ${logs}`},
+        });
+        responseWorks = await fetch("http://localhost:5678/api/works");
+        projects = await responseWorks.json();
+        const divGallery = document.querySelector(".gallery");
+        const projetElement = document.createElement("figure");
+        projetElement.dataset.category = projects[projects.length - 1].category.id;
+        const imgElement = document.createElement("img");
+        imgElement.src = projects[projects.length - 1].imageUrl;
+        const titleElement = document.createElement("figcaption");
+        titleElement.textContent = projects[projects.length - 1].title;
+        divGallery.appendChild(projetElement);
+        projetElement.appendChild(imgElement);
+        projetElement.appendChild(titleElement);
     })
-};
-*/
+    
+}
 
 export {
     generrateportfolio,
@@ -170,4 +200,6 @@ export {
     generateGridPopUp,
     removeProject,
     displayAddProjectPopUp,
+    addImage,
+    addProject,
 };
--- assets/JS/portfolio.js
+++ assets/JS/portfolio.js
@@ -9,14 +9,15 @@ import {
     generateGridPopUp,
     removeProject,
     displayAddProjectPopUp,
+    addImage,
+    addProject,
 } from "./functions.js";
-
 
 const responseWorks = await fetch("http://localhost:5678/api/works");
 const projects = await responseWorks.json();
+
 const responseCategories = await fetch("http://localhost:5678/api/categories");
 const categories = await responseCategories.json();
-
 let logs = window.localStorage.getItem("logs");
 const divGallery = document.querySelector(".gallery");
 const portfolio = document.querySelector("#portfolio");
@@ -24,6 +25,10 @@ const btnCategories = document.querySelectorAll(".btn");
 const btnAll = document.querySelector(".filtres .btn:first-child");
 const logIn = document.querySelector('nav a[href="./login.html"]');
 const popupBackground = document.querySelector(".popupBackground");
+const addProjectForm = document.getElementById("add_project");
+const inputAddImage = document.querySelector("[type=file]");
+const DivPreviewImage = document.querySelector(".add_picture");
+
 
 generrateportfolio(projects, divGallery);
 
@@ -36,41 +41,8 @@ if (logs != null) {
     generateGridPopUp(projects);
     removeProject(projects,logs);
     displayAddProjectPopUp(popupBackground);
+    addImage(inputAddImage, DivPreviewImage);
+    addProject(addProjectForm, logs, projects);
 } else {
     filterButton(categories, btnCategories, btnAll);
 };
-
-const addProjectForm = document.getElementById("add_project");
-const inputAddImage = document.querySelector("[type=file]");
-const DivPreviewImage = document.querySelector(".add_picture");
-inputAddImage.addEventListener("change", (event) => {
-    const image = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = (event) => {
-        const divAddPictureContent = document.querySelectorAll(".add_picture *");
-        divAddPictureContent.forEach(element => {
-            element.style.display = "none";
-        });
-        const previewImage = document.createElement("img");
-        previewImage.src = event.target.result;
-        DivPreviewImage.appendChild(previewImage);
-        DivPreviewImage.style.padding = 0;
-    };
-    reader.readAsDataURL(image);
-});
-
-addProjectForm.addEventListener("submit", async function (event) {
-    event.preventDefault();
-    const newProject = new FormData();
-        newProject.append("image", event.target.querySelector("[name=image]").files[0]);
-        newProject.append("title", event.target.querySelector("[name=title]").value);
-        newProject.append("category", event.target.querySelector("[name=category]").value);
-    const POSTrequest = await fetch("http://localhost:5678/api/works", {
-        method: "POST",
-        body: newProject,
-        headers: {"Authorization": `Bearer ${logs}`},
-    });
-    console.log(newProject);
-    if (POSTrequest.status === 200) {console.log("Success!");}
-    else {console.log("Erreur")};
-})
